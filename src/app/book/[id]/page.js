666ebf6e289ee6a7eca1266e88d11982e3f3357c
abdulsalamibrahimyakubu6-hub <abdulsalamibrahimyakubu6@gmail.com>
2026-07-08 11:57:01 +0100

    Text params

--- src/app/book/[id]/page.js
+++ src/app/book/[id]/page.js
@@ -1,82 +1,8 @@
-import Link from "next/link";
-
-export default async function BookPage({ params }) {
-  console.log("Params:", params);
-
-  const { id } = await params;
-
-  console.log("Book ID:", id);
-
-
-  const response = await fetch(
-    `https://gutendex.com/books/${id}`
-  );
-
-  const book = await response.json();
-
-  const imageUrl = book.formats["image/jpeg"];
-  const author =
-    book.authors?.[0]?.name || "Unknown Author";
-
-  const language =
-    book.languages?.join(", ").toUpperCase() || "N/A";
-
-  const downloads = book.download_count || 0;
-
-  const readLink =
-    book.formats["text/html"] ||
-    book.formats["application/epub+zip"] ||
-    book.formats["application/pdf"];
-
+export default async function BookPage({ params} ) {
   return (
-    <div className="book-details">
-
-      <h1>{book.title}</h1>
-
-      {imageUrl && (
-        <img
-          src={imageUrl}
-          alt={book.title}
-          className="details-image"
-        />
-      )}
-
-      <div className="book-info">
-
-        <p>
-          <strong>✍️ Author:</strong> {author}
-        </p>
-
-        <p>
-          <strong>🌍 Language:</strong> {language}
-        </p>
-
-        <p>
-          <strong>⬇️ Downloads:</strong> {downloads}
-        </p>
-
-      </div>
-
-      {readLink ? (
-        <a
-          href={readLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="read-btn"
-        >
-          📖 Read Book
-        </a>
-      ) : (
-        <p>No online version available.</p>
-      )}
-
-      <br />
-      <br />
-
-      <Link href="/" className="read-btn">
-        🏠 Back Home
-      </Link>
-
-    </div>
+    <pre>
+      {JSON.stringify(params, null, 2)}
+    </pre>
   );
+  
 }
